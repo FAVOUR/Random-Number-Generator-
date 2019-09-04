a2638cc1ef947cdc06ea3f4251fde072f3d003b6
favour <truelyfavoured@gmail.com>
2019-09-04 08:36:28 +0100

    Initial commit for binding a service to an activity

--- app/src/main/java/com/example/services/MainActivity.java
+++ app/src/main/java/com/example/services/MainActivity.java
@@ -4,17 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button startService;
     Button stopService;
+    Button bindService;
+    Button unbindService;
+    Button randomNumber;
+
     TextView display;
+
+    MyService myService;
+    private Boolean isServiceBound;
+    ServiceConnection serviceConnection;
+
+
+
 
 
     @Override
@@ -28,33 +42,52 @@ public class MainActivity extends AppCompatActivity {
        startService = (Button) findViewById(R.id.start_service);
        display=(TextView)findViewById(R.id.display) ;
         stopService=(Button) findViewById(R.id.stop_service);
+        bindService = (Button) findViewById(R.id.bind);
+        unbindService=(Button) findViewById(R.id.unbind) ;
+        randomNumber=(Button) findViewById(R.id.getRandomNumber);
 
 
-
-        startService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent serviceIntent = new Intent(MainActivity.this,MyService.class);
-
-                startService(serviceIntent);
-            }
-        });
-
-
-        stopService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent serviceIntent = new Intent(MainActivity.this,MyService.class);
-
-
-                stopService(serviceIntent);
-            }
-        });
+        startService.setOnClickListener(this);
+         stopService.setOnClickListener(this);
+         bindService.setOnClickListener(this);
+         unbindService.setOnClickListener(this);
+         randomNumber.setOnClickListener(this);
 
 
 
     }
 
 
+    @Override
+    public void onClick(View view) {
+        Intent serviceIntent = new Intent(MainActivity.this,MyService.class);
+
+        switch (view.getId()){
+            case R.id.start_service:{
+                startService(serviceIntent);
+            }
+            break ;
+            case R.id.stop_service:{
+                stopService(serviceIntent);
+
+            }
+            break ;
+            case R.id.bind:{
+
+            }
+            break ;
+            case R.id.unbind:{
+
+            }
+            break ;
+            case R.id.getRandomNumber:{
+
+            }
+            break ;
+
+            default:
+                Toast.makeText(this,"You have selected Nothing ",Toast.LENGTH_SHORT).show();
+
+        }
+    }
 }
